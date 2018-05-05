e72fb736a2e3349b1bc69ecaa27e076d766bb641
Andrey Dobrynin <andrdobrynin@gmail.com>
2018-05-05 17:33:49 +0700

    Init test task

---
+++ .section.php
@@ -0,0 +1,9 @@
+<?
+$sSectionName = 'Главная';
+$arDirProperties = array(
+	'title' => 'Тестовое задание ',
+	'description' => 'Тестовое задание ',
+	'keywords' => 'Тестовое задание ',
+	'robots' => 'index, follow'
+);
+?>
